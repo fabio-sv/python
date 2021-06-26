b1a6240115f65d12a953fee747eeacc9fc87af56
fabio-sv <fabio_dev@outlook.com>
2021-06-26 15:35:55 -0300

    update

---
+++ tupla/junte_as_tuplas.py
@@ -0,0 +1,12 @@
+"""Junte-se a Duas Tuplas
+Para participar de duas ou mais tuplas você pode usar o operador:+ """
+
+#Junte-se a duas tuplas:
+tupla =  ('a', 'b', 'c')
+tupla2  = (1, 2, 3)
+
+tupla3 = tupla + tupla2;
+
+print(tupla3)
+
+#('a', 'b', 'c', 1, 2, 3)
---
+++ tupla/multiplique_tuplas.py
@@ -0,0 +1,10 @@
+'''Se você quiser multiplicar o conteúdo de um tupla um determinado número de vezes, 
+você pode usar o operador: * '''
+
+#Multiplique as frutas tuple por 2:
+
+tupla  = ('maçã', 'banana', 'laranja')
+tupla2 = tupla * 2;
+
+print(tupla2)
+#('maçã', 'banana', 'laranja', 'maçã', 'banana', 'laranja')
